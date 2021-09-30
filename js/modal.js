9e3d52b2ff48bb62c0bca1e07504f60f385fee6f
AlexeiKokarev <87031896+AlexeiKokarev@users.noreply.github.com>
2021-09-30 13:52:10 +0300

    123546

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,14 @@
 (() => {
     const refs = {
-        openModalBtn: document.querySelector('[data-modal-open]'),
-        closeModalBtn: document.querySelector('[data-modal-close]'),
-        modal: document.querySelector('[data-modal]'),
+        openModalBtn: document.querySelector('hero__button'),
+        closeModalBtn: document.querySelector('window__modal--button'),
+        modal: document.querySelector('window'),
     };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
 
     function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
+        refs.modal.classList.toggle('window__is--hidden');
     }
 })();
